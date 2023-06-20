8c3f35fd536fe7d932c0543faa928b5e56458c6b
Nexuslolz <a.klyosov@inbox.ru>
2023-06-20 21:24:21 +0500

    feature

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/todo-list-testing/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/todo-list-testing/'
+    : '/'
 });
